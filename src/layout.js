4387e3011b3266b78b42056deda27158bdd4f712
Michael McDermott <mmcdermott@mad-computer-scientist.com>
2017-05-27 14:11:12 -0500

    Starting to fiddle with some ideas.

---
+++ src/layout.js
@@ -0,0 +1,25 @@
+"use strict";
+import 'babel-polyfill';
+
+class Diagram {
+    constructor() {
+        this.layers = [];
+    }
+
+    addLayer(l) {
+        this.layers.push(l);
+    }
+}
+
+class Grid {
+    constructor() {
+    }
+
+    addShape() {
+    }
+}
+
+class Shape {
+    constructor() {
+    }
+}
