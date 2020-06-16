771c398b806965a08963cc0a3f7776979dd759ba
Michael McDermott <michael.mcdermott@jbhunt.com>
2020-06-16 17:00:36 -0500

    Started sketching out what a rendering core might look like.

---
+++ src/main/clojure/venn/core.clj
@@ -0,0 +1,36 @@
+(ns venn.core
+  (:require [clojure.data.xml :refer :all :as xml]))
+
+(def a 
+ {:venn/diagram "my-pic"
+  :venn/layers [
+     {:grid/layer "asdf" 
+        :venn/depth 1 
+        :grid/rows 20 
+        :grid/columns 5 
+        :venn/contents [ 
+            {:shapes/rectangle "100" :grid/column 1 :grid/row 1 } 
+        ]
+     }
+     {:float/layer "100"
+      :venn/depth 2
+     }
+ ]})
+
+(defn render-layers [diagram]
+ (xml/element "g" {}
+     (let [ls (sort (comp > :venn/depth) (:venn/layers diagram))]
+        (map (fn [l]
+         (xml/element "g" {}
+          (xml/element "title" {}
+           (or (:grid/layer l)
+               (:float/layer l))
+          )))
+         ls)
+     )
+))
+
+(defn venn->svg [diagram]
+ (xml/emit-str 
+  (xml/element "svg" {}
+                (render-layers diagram))))
