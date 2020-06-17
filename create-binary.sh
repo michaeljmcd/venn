aae5729189c0efd2bd85f5a893c7f2cdf994ea29
Michael McDermott <mcdermott.michaelj@gmail.com>
2020-06-17 08:40:30 -0500

    Fiddling with native image generation some more.

--- create-binary.sh
+++ create-binary.sh
@@ -7,4 +7,7 @@ rm -rf classes/*
 clj -e "(compile 'venn.core)"
 clj -A:uberjar --main-class venn.core
 
-native-image -jar target/venn.jar
+# See: https://github.com/oracle/graal/issues/1265
+native-image -Dclojure.compiler.direct-linking=true -jar target/venn.jar --no-fallback --report-unsupported-elements-at-runtime --initialize-at-build-time --enable-url-protocols=http,https
+
+#mv venn target/
--- src/main/clojure/venn/core.clj
+++ src/main/clojure/venn/core.clj
@@ -2,21 +2,21 @@
   (:require [clojure.data.xml :refer :all :as xml])
   (:gen-class))
 
-(def a 
- {:venn/diagram "my-pic"
-  :venn/layers [
-     {:grid/layer "asdf" 
-        :venn/depth 1 
-        :grid/rows 20 
-        :grid/columns 5 
-        :venn/contents [ 
-            {:shapes/rectangle "100" :grid/column 1 :grid/row 1 } 
-        ]
-     }
-     {:float/layer "100"
-      :venn/depth 2
-     }
- ]})
+(defn create-sample []
+    {:venn/diagram "my-pic"
+      :venn/layers [
+         {:grid/layer "asdf" 
+            :venn/depth 1 
+            :grid/rows 20 
+            :grid/columns 5 
+            :venn/contents [ 
+                {:shapes/rectangle "100" :grid/column 1 :grid/row 1 } 
+            ]
+         }
+         {:float/layer "100"
+          :venn/depth 2
+         }
+     ]})
 
 (defn render-layers [diagram]
  (xml/element "g" {}
@@ -36,6 +36,6 @@
   (xml/element "svg" {}
                 (render-layers diagram))))
 
-(defn -main [& args]
+(defn -main []
  (print "I am here")
- (spit "a.svg" (venn->svg a)))
+ (spit "a.svg" (venn->svg (create-sample))))
